2586132ddb7ca2232b844b1e6ec8f8dfd33e0408
Maria Jose Travez <mtravez@3-H-3.42heilbronn.de>
2026-05-28 16:15:45 +0200

    First compileable commit

--- include/miniRT.h
+++ include/miniRT.h
@@ -1,10 +1,30 @@
-// main struct for calculations
-// left handed coord-system
-// x -> right, y -> up, z -> away from you, w = 0 -> vector, w = 1 -> point (could be bool idk?)
-// points are positions in space, vectors represent direction + distance
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   miniRT.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtravez <mtravez@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/28 16:06:41 by mtravez           #+#    #+#             */
+/*   Updated: 2026/05/28 16:14:02 by mtravez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include "../MLX42/include/MLX42/MLX42.h"
+# include "../libft/libft.h"
+# include <math.h>
+# include <stdio.h>
+# define WIN_H 750
+# define WIN_W 1024
+
 typedef struct s_tuple {
 	double x;
 	double y;
 	double z;
 	double w; 
 }	t_tuple;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "../include/miniRT.h"
+
+int main(int argc, char *argv[])
+{
+    if (argv != NULL) {
+        argc = 1;
+    }
+    printf("Hello world!");
+    return 0;
+}
