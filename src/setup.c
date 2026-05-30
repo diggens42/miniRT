e271a5b983e149c35f8760f7a8f2e146aa404cfc
diggens42 <frederic.marcel.wahl@gmail.com>
2026-05-30 19:45:55 +0200

    open mlx window  and loop etc

--- src/setup.c
+++ src/setup.c
@@ -1,27 +1,21 @@
 #include "../include/miniRT.h"
 
-int	play(t_data data)
+int	run_mlx(t_rt *rt)
 {
 	mlx_t		*mlx;
-	t_player	player;
-	t_map3d		map3d;
+	mlx_image_t	*image;
 
-	mlx = mlx_init(WIN_W, WIN_H, "Game", false);
-	player.mlx = mlx;
-	player.map3d = &map3d;
-	set_player(&data, &player);
-	if (!player.textures[0])
+	mlx = mlx_init(W_WIDTH, W_HEIGHT, "miniRT", false);
+	if (!mlx)
+		return (1);
+	image = mlx_new_image(mlx, W_WIDTH, W_HEIGHT);
+	if (!image)
 	{
 		mlx_terminate(mlx);
 		return (1);
 	}
-	mlx_image_to_window(mlx, player.img, 0, 0);
-	draw_rays_3d(player);
-	mlx_key_hook(mlx, &buttons, &player);
+	mlx_image_to_window(mlx, image, 0, 0);
 	mlx_loop(mlx);
-	mlx_delete_image(mlx, player.img);
-	delete_textures(&player);
-	free_map(&map3d);
 	mlx_terminate(mlx);
 	return (0);
 }
