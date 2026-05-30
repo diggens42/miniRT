982b997bf31f1b5de589aca4c1764bf2c2032c10
diggens42 <frederic.marcel.wahl@gmail.com>
2026-05-30 19:05:15 +0200

    added color_ops helper functions, changed rgb vals to doubles so we dont have problems with lighting stuff later

---
+++ src/utils/color_ops.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color_ops.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwahl <fwahl@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/30 18:42:04 by fwahl             #+#    #+#             */
+/*   Updated: 2026/05/30 19:02:26 by fwahl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/miniRT.h"
+
+t_rgb	new_rgb(double r, double g, double b)
+{
+	t_rgb	color;
+
+	color.r = r;
+	color.g = g;
+	color.b = b;
+	return (color);
+}
+
+t_rgb	rgb_add(t_rgb color_a, t_rgb color_b)
+{
+	return (new_rgb(color_a.r + color_b.r,
+			color_a.g + color_b.g,
+			color_a.b + color_b.b));
+}
+
+t_rgb	rgb_subtract(t_rgb color_a, t_rgb color_b)
+{
+	return (new_rgb(color_a.r - color_b.r,
+			color_a.g - color_b.g,
+			color_a.b - color_b.b));
+}
+
+t_rgb	rgb_multiply(t_rgb color_a, double scalar)
+{
+	return (new_rgb(color_a.r * scalar,
+			color_a.g * scalar,
+			color_a.b * scalar));
+}
+
+t_rgb	rgb_hadamard(t_rgb color_a, t_rgb color_b)
+{
+	return (new_rgb(color_a.r * color_b.r,
+			color_a.g * color_b.g,
+			color_a.b * color_b.b ));
+}
