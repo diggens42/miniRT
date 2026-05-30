3871f24c928c5037c0ddf6c2e3e0b3f33405f193
diggens42 <frederic.marcel.wahl@gmail.com>
2026-05-30 18:04:51 +0200

    add doc comments for vec_ops

--- src/utils/tuple_vec_ops.c
+++ src/utils/tuple_vec_ops.c
@@ -6,18 +6,21 @@
 /*   By: fwahl <fwahl@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 17:05:19 by fwahl             #+#    #+#             */
-/*   Updated: 2026/05/30 17:58:35 by fwahl            ###   ########.fr       */
+/*   Updated: 2026/05/30 18:04:14 by fwahl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/miniRT.h"
 
+/** returns len of vec 
+ *  sqrt(x² + y² + z² + w²) */
 double	magnitude(t_tuple vec)
 {
 	return (sqrt(vec.x * vec.x + vec.y * vec.y
 			+ vec.z * vec.z + vec.w * vec.w));
 }
 
+/** returns vec scaled to len = 1 (unit vec, same direction) */
 t_tuple	normalize(t_tuple vec)
 {
 	double	mag;
@@ -26,12 +29,17 @@ t_tuple	normalize(t_tuple vec)
 	return (new_tuple(vec.x / mag, vec.y / mag, vec.z / mag, vec.w / mag));
 }
 
+/** dot product - measures alignment between two vecs
+ *  > 0 same direction
+ *  < 0 opposite direction
+ *    0 perpendicular */
 double	dot(t_tuple vec_a, t_tuple vec_b)
 {
 	return (vec_a.x * vec_b.x + vec_a.y * vec_b.y
 		+ vec_a.z * vec_b.z + vec_a.w * vec_b.w);
 }
 
+/** cross product - returns vec perpendicular to both inputs (right-hand rule) */
 t_tuple	cross(t_tuple vec_a, t_tuple vec_b)
 {
 	return (new_vec(vec_a.y * vec_b.z - vec_a.z * vec_b.y,
