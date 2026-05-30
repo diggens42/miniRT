4d01fc28e433abec35f644b58d472290a0366672
diggens42 <frederic.marcel.wahl@gmail.com>
2026-05-30 19:51:57 +0200

    render thingy

--- src/setup.c
+++ src/setup.c
@@ -19,3 +19,25 @@ int	run_mlx(t_rt *rt)
 	mlx_terminate(mlx);
 	return (0);
 }
+
+void	render(t_rt *rt, mlx_image_t *image)
+{
+	t_rgb		color;
+	uint32_t	pixel;
+	int			x;
+	int			y;
+
+	color = rt->alight.rgb;
+	pixel = color_to_pixel(color);
+	y = 0;
+	while (y < W_HEIGHT)
+	{
+		x = 0;
+		while (x < W_WIDTH)
+		{
+			mlx_put_pixel(image, x, y, pixel);
+			x++;
+		}
+		y++;
+	}
+}
